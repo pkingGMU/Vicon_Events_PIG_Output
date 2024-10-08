1b16d2d91dcaf88b0fe9a432247531c2c887171d
Patrick King <pat.k195@gmail.com>
2024-10-08 17:07:06 -0400

    Events are output to its own table matching the output of a vicon export

--- Functions/arrange_tables.m
+++ Functions/arrange_tables.m
@@ -1,4 +1,4 @@
-function [proc_tables] = arrange_tables(folder)
+function [proc_tables, event_table] = arrange_tables(folder)
     %%%
     % 
     %%%
@@ -70,7 +70,72 @@ function [proc_tables] = arrange_tables(folder)
         
         proc_tables.(file_name_short).trajectory_data_table = table_processing('Trajectories', full_data_table);
         
-       
+        %%% GAIT EVENTS
+
+        [lhs,lto,rhs,rto] = gait_detection(proc_tables.(file_name_short).trajectory_data_table);
+
+        % Define the subject
+        [~,subject_id, ~] = fileparts(folder);
+        
+        % Find max length of events
+        max_left = max([length(lhs), length(lto)]);
+        max_right = max([length(rhs), length(rto)]);
+
+        % Pad shorter arrays with NaN
+        lhs = [lhs; NaN(max_left - length(lhs), 1)];
+        lto = [lto; NaN(max_left - length(lto), 1)];
+        rhs = [rhs; NaN(max_right - length(rhs), 1)];
+        rto = [rto; NaN(max_right - length(rto), 1)];
+
+        % Total number of rows (for left and right events)
+        total_rows = (max_left + max_right) * 2;
+        
+        % Prepare a cell array for output (4 columns: Subject ID, Side, Event, Value, Description)
+        output_data = cell(total_rows, 5);
+        
+        % Fill in the left side events first (alternating between heel strike and toe off)
+        row_counter = 1;
+        for i = 1:max_left
+            % Left heel strike
+            output_data{row_counter, 1} = subject_id;
+            output_data{row_counter, 2} = 'Left';
+            output_data{row_counter, 3} = 'Heel Strike';
+            output_data{row_counter, 4} = lhs(i);
+            row_counter = row_counter + 1;
+            
+            % Left toe off
+            output_data{row_counter, 1} = subject_id;
+            output_data{row_counter, 2} = 'Left';
+            output_data{row_counter, 3} = 'Toe Off';
+            output_data{row_counter, 4} = lto(i);
+            row_counter = row_counter + 1;
+        end
+        
+        % Fill in the right side events (alternating between heel strike and toe off)
+        for i = 1:max_right
+            % Right heel strike
+            output_data{row_counter, 1} = subject_id;
+            output_data{row_counter, 2} = 'Right';
+            output_data{row_counter, 3} = 'Heel Strike';
+            output_data{row_counter, 4} = rhs(i);
+            row_counter = row_counter + 1;
+            
+            % Right toe off
+            output_data{row_counter, 1} = subject_id;
+            output_data{row_counter, 2} = 'Right';
+            output_data{row_counter, 3} = 'Toe Off';
+            output_data{row_counter, 4} = rto(i);
+            row_counter = row_counter + 1;
+        end
+
+        % Convert to table
+        event_table = cell2table(output_data, 'VariableNames', {'Subject', 'Context', 'Name', 'Time (s)', 'Description'});
+
+
+     
+
+
+
         
      
     end
