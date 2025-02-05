89bc50bd61a25505d4ac4f43b5c49ba66af67d18
pking23 <pking23@gmu.edu>
2025-02-05 12:39:51 -0500

    Added proper routing for Obstacle Crossing Outcomes

--- Functions/selection_routing.m
+++ Functions/selection_routing.m
@@ -70,6 +70,27 @@ function selection_routing(outcome_selection, fr)
             
 
         case 'Obstacle Crossing Outcomes'
+            [folderNames, dataPath, choice] = folder_names(1);
+
+            % Display list dialog to select subject folders
+            if isempty(folderNames)
+                uialert(uifigure, 'No folders found in Data directory.', 'Folder Error');
+            else
+                [selection, ok] = listdlg('PromptString', 'Select Subject Folders:', ...
+                                          'SelectionMode', 'multiple', ...
+                                          'ListString', folderNames);
+
+                % If OK and made a selection
+                if ok
+                    selectedFolders = fullfile(dataPath, folderNames(selection));
+                    disp('Selected folders for processing:');
+                    disp(selectedFolders);
+                else
+                    disp('No folders selected.');
+                end
+            end
+
+            obstacle_process(selectedFolders, choice, fr)
         otherwise
             
 
