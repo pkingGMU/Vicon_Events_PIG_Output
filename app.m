4af64a39f14ed5687a52a2891640d1357575d3b9
pking23 <pking23@gmu.edu>
2025-03-19 18:49:32 -0400

    Initialization of main application window

--- app.m
+++ app.m
@@ -28,47 +28,60 @@ function app()
 
     r01preset;
 
-    
+    %%% TODO BATCH
+
+    r01.intern.batchmode = 0;
+    r01logo;
+
+    % Allow for loading
+    pause(1);
+    delete(r01.gui.fig_logo);
+
+    r01gui;
+
+    add2log(0,['>>>> ',datestr(now,31), ' Session started'],1,1);
+
+
 
     % Create a figure for the UI
-    fig = uifigure('Name', 'Data Processing App', 'Position', [100, 100, 300, 300]);
+    %fig = uifigure('Name', 'Data Processing App', 'Position', [100, 100, 300, 300]);
 
     % Create a button for "Process Data"
-    btnProcessData = uibutton(fig, 'push', 'Text', 'Start', ...
-        'Position', [50, 180, 200, 40], ...
-        'ButtonPushedFcn', @(btn,event) processDataCallback(fig));
-
-    % Create a text field for Frame Rate input
-    lblFrameRate = uilabel(fig, 'Text', 'Frame Rate:', 'Position', [50, 80, 80, 22]);
-    txtFrameRate = uieditfield(fig, 'numeric', 'Position', [130, 80, 100, 22]);
-    txtFrameRate.Value = 100;  % Default value is 100
-
-end
-
-% Callback function for Process Data button
-function processDataCallback(fig)
-    
-    % Get the frame rate input from the text box
-    frameRate = fig.Children(1).Value;  % Access the text box for frame rate
-    if isnan(frameRate)
-        uialert(fig, 'Please enter a valid frame rate value.', 'Input Error');
-        return;
-    end
-
-    
-    possible_outcomes = ["Gait Events", "Gait Events & Clean Force Strikes", "R01 Analysis", ...
-        "Obstacle Crossing Outcomes", "Margin Of Stability"];
-    
-    % Get list of outcome measures
-    [outcome_selection, ~] = listdlg('PromptString', 'Select Outcome:', ...
-                              'SelectionMode', 'single', ...
-                              'ListString', possible_outcomes);
-
-    outcome_selection = possible_outcomes(outcome_selection);
-
-    selection_routing(outcome_selection, frameRate);
-
-    disp("Finished!")
-
-end
+%     btnProcessData = uibutton(fig, 'push', 'Text', 'Start', ...
+%         'Position', [50, 180, 200, 40], ...
+%         'ButtonPushedFcn', @(btn,event) processDataCallback(fig));
+% 
+%     % Create a text field for Frame Rate input
+%     lblFrameRate = uilabel(fig, 'Text', 'Frame Rate:', 'Position', [50, 80, 80, 22]);
+%     txtFrameRate = uieditfield(fig, 'numeric', 'Position', [130, 80, 100, 22]);
+%     txtFrameRate.Value = 100;  % Default value is 100
+% 
+% end
+% 
+% % Callback function for Process Data button
+% function processDataCallback(fig)
+% 
+%     % Get the frame rate input from the text box
+%     frameRate = fig.Children(1).Value;  % Access the text box for frame rate
+%     if isnan(frameRate)
+%         uialert(fig, 'Please enter a valid frame rate value.', 'Input Error');
+%         return;
+%     end
+% 
+% 
+%     possible_outcomes = ["Gait Events", "Gait Events & Clean Force Strikes", "R01 Analysis", ...
+%         "Obstacle Crossing Outcomes", "Margin Of Stability"];
+% 
+%     % Get list of outcome measures
+%     [outcome_selection, ~] = listdlg('PromptString', 'Select Outcome:', ...
+%                               'SelectionMode', 'single', ...
+%                               'ListString', possible_outcomes);
+% 
+%     outcome_selection = possible_outcomes(outcome_selection);
+% 
+%     selection_routing(outcome_selection, frameRate);
+% 
+%     disp("Finished!")
+% 
+% end
 
