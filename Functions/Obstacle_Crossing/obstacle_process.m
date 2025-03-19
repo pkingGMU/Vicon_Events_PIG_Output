d71111103f952d291fc96be29d704aac568ec3af
pking23 <pking23@gmu.edu>
2025-03-19 10:48:42 -0400

    Support for export of one excel file for obstacle crossing

--- Functions/Obstacle_Crossing/obstacle_process.m
+++ Functions/Obstacle_Crossing/obstacle_process.m
@@ -3,12 +3,14 @@ function [subjects] = obstacle_process(subjects_list, choice, fr)
 % Takes in the list of subjects and arranges the data for each one. Returns
 % the struct 'subjects' for each subject for each file.
 
+    total_OBS = [];
+
     % Each subject folder
     for i = 1:length(subjects_list)
         subject = subjects_list(i);
 
         % Get subject data for subject folder
-        [proc_tables] = obstacle_arrange_tables(subject, choice, fr);
+        [proc_tables, total_OBS] = obstacle_arrange_tables(subject, choice, fr, total_OBS);
 
         %%% new arrange table for Obstacle Crossing?
     
@@ -27,4 +29,27 @@ function [subjects] = obstacle_process(subjects_list, choice, fr)
         subjects.(subject).proc_tables = proc_tables;
 
     end
+
+    % ***************** Export data to an Excel sheet ***********************
+    % Name the excel sheet: (with file path)
+    fname2 = fullfile('Output', 'Obstacle_Crossing', strcat('total', '.xlsx'));
+    % headers = {'Trial','Lead Foot','Obstacle_approach_dist_trail','Obstacle_landing_dist_lead',...
+    %     'Obstacle_approach_dist_lead','Obstacle_landing_dist_trail',...
+    %     'Lead_toe_clearance','Trail_toe_clearance','Lead_heel_clearance','Trail_heel_clearance',...
+    %     'Obstacle Height', 'Start', 'End', 'Lead Step Length', 'Trail Step Length', 'Lead Step Width'...
+    %     , 'Trail Step Width', 'LMoS_AP_hs','RMoS_AP_hs','LMoS_ML_hs','RMoS_ML_hs','LMoS_AP_to','RMoS_AP_to',...
+    %     'LMoS_ML_to','RMoS_ML_to'};
+
+    headers = {'Subject', 'Trial','Lead Foot','Obstacle_approach_dist_trail','Obstacle_landing_dist_lead',...
+        'Obstacle_approach_dist_lead','Obstacle_landing_dist_trail',...
+        'Lead_toe_clearance','Trail_toe_clearance','Lead_heel_clearance','Trail_heel_clearance',...
+        'Obstacle Height', 'Start', 'End', 'Lead Step Length', 'Trail Step Length', 'Lead Step Width'...
+        , 'Trail Step Width', 'LMoS_AP_Double_Before','RMoS_AP_Double_Before','LMoS_ML_Double_Before','RMoS_ML_Double_Before','LMoS_AP_Double_After','RMoS_AP_Double_After',...
+        'LMoS_ML_Double_After','RMoS_ML_Double_After'};
+    
+
+    % Convert OBS_data to a table
+    OBS_table = cell2table(total_OBS, 'VariableNames', headers);
+
+    writetable(OBS_table, fname2, 'WriteRowNames', false);
 end
